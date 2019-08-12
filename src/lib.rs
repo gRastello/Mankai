2dd86429a6822661912e5db17abc64d02112324f
Gabriele Rastello <grastello@localhost.localdomain>
2019-08-12 14:54:03 +0200

    Add numbers and identifiers and test

--- src/lib.rs
+++ src/lib.rs
@@ -83,7 +83,20 @@ impl Lexer {
 
     /// Peek the next character without advancing the lexer.
     fn peek(&self) -> char {
-        self.source.chars().nth(self.current).unwrap()
+        if self.is_at_end() {
+            '\0'
+        } else {
+            self.source.chars().nth(self.current).unwrap()
+        }
+    }
+
+    /// Peek the character adter the next one.
+    fn peek_next(&self) -> char {
+        if self.is_at_end() {
+            '\0'
+        } else {
+            self.source.chars().nth(self.current + 1).unwrap()
+        }
     }
 
     /// Add a new token to the internal store with the given kind.
@@ -125,6 +138,48 @@ impl Lexer {
         Ok(())
     }
 
+    /// Tokenize a number.
+    fn finish_number(&mut self) -> Result<(), ScanError> {
+        // Consume the non-decimal part.
+        while self.peek().is_digit(10) {
+            self.current += 1;
+        }
+
+        // Check if the number has a decimal part.
+        if self.peek() == '.' && self.peek_next().is_digit(10) {
+            // Consume the '.'.
+            self.current += 1;
+
+            // Consume the decimal part.
+            while self.peek().is_digit(10) {
+                self.current += 1;
+            }
+        }
+
+        let number: f64 = self
+            .source
+            .chars()
+            .skip(self.start)
+            .take(self.current - self.start)
+            .collect::<String>()
+            .parse()
+            .unwrap();
+        self.add_token(TokenKind::Number(number));
+
+        Ok(())
+    }
+
+    /// Tokenize an identifier.
+    fn finish_identifier(&mut self) -> Result<(), ScanError> {
+        while !self.peek().is_whitespace() && !self.is_at_end() {
+            self.current += 1;
+        }
+
+        self.add_token(TokenKind::Identifier);
+
+        Ok(())
+    }
+
     /// Scan a new token.
     fn scan_token(&mut self) -> Result<(), ScanError> {
         self.start = self.current;
@@ -139,7 +194,13 @@ impl Lexer {
             '(' => Ok(self.add_token(TokenKind::LeftParen)),
             ')' => Ok(self.add_token(TokenKind::RightParen)),
             '"' => self.finish_string(),
-            _ => Ok(()),
+            _ => {
+                if c.is_digit(10) {
+                    self.finish_number()
+                } else {
+                    self.finish_identifier()
+                }
+            }
         }
     }
 
@@ -172,13 +233,28 @@ mod test {
 
     #[test]
     fn lexing() {
-        let mut lexer = Lexer::new("(+-) */   \n  \"foo\" +");
+        let mut lexer = Lexer::new("(+-) */   \n  \"foo\" 64.333 12 foo");
         let mut token;
 
         lexer.scan();
 
         token = lexer.tokens.pop().unwrap();
-        assert_eq!(token, Token::new(String::from("+"), TokenKind::Plus));
+        assert_eq!(
+            token,
+            Token::new(String::from("foo"), TokenKind::Identifier)
+        );
+
+        token = lexer.tokens.pop().unwrap();
+        assert_eq!(
+            token,
+            Token::new(String::from("12"), TokenKind::Number(12.0))
+        );
+
+        token = lexer.tokens.pop().unwrap();
+        assert_eq!(
+            token,
+            Token::new(String::from("64.333"), TokenKind::Number(64.333))
+        );
 
         token = lexer.tokens.pop().unwrap();
         assert_eq!(
