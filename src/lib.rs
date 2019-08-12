bb50c47d72b7fb93c58bf3bf58a103cabbcfabfd
Gabriele Rastello <grastello@localhost.localdomain>
2019-08-12 12:46:22 +0200

    Add TokenKind enum and Token struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+/// Types of tokens.
+enum TokenKind {
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    String(String),
+    Number(f64),
+    LeftParen,
+    RightParen,
+    Eof,
+}
+
+/// A token.
+struct Token {
+    /// Corresponding lexeme.
+    lexeme: String,
+    /// Kind of the token.
+    kind: TokenKind,
+}
+
+impl Token {
+    /// Create a new token from lexeme and kind.
+    fn new(lexeme: &str, kind: TokenKind) -> Self {
+        Token {
+            lexeme: String::from(lexeme),
+            kind,
+        }
+    }
+}
+
 pub fn test_function() {
     println!("foo bar");
 }
