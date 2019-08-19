c5f91b9a8a2660f46abf0074a91fae07878a72e1
Gabriele Rastello <grastello@localhost.localdomain>
2019-08-19 20:32:01 +0200

    Better error messages for '+'

--- src/native_functions.rs
+++ src/native_functions.rs
@@ -1,14 +1,21 @@
 use crate::interpreter::{MankaiObject, RuntimeError};
 
-/// Sum all the arguments. Return an error if a non numeric argument is found.
+/// Sum all the arguments. Return an error if a non numeric argument is found
+/// or no arguments are found at all.
 pub fn sum(arguments: Vec<MankaiObject>) -> Result<MankaiObject, RuntimeError> {
+    // Check arity.
+    if arguments.is_empty() {
+        return Err(RuntimeError::new("'+' requires at least one argument!"));
+    }
+
+    // Perform the sum.
     let mut sum = 0.0;
     for (i, value) in arguments.iter().enumerate() {
         match value {
             MankaiObject::Number(n) => sum += n,
             _ => {
                 return Err(RuntimeError::new(&format!(
-                    "{}-th argument is not a number!",
+                    "{}-th argument of '+' is not a number!",
                     i + 1
                 )))
             }
